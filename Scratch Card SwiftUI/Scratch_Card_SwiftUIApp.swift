e632134f6a11d0933837012507c4da819e324168
neshwa.parveen <neshwa.parveen@chillarcards.com>
2023-12-06 17:47:37 +0530

    Initial Commit

---
+++ Scratch Card SwiftUI/Scratch_Card_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scratch_Card_SwiftUIApp.swift
+//  Scratch Card SwiftUI
+//
+//  Created by Neshwa on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Scratch_Card_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
