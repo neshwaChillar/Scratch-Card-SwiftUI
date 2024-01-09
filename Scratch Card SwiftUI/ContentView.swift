ac45fa8a7c1476f39b21adae948aae1e6e98939b
neshwa.parveen <neshwa.parveen@chillarcards.com>
2024-01-09 15:18:20 +0530

    initial commit

--- Scratch Card SwiftUI/ContentView.swift
+++ Scratch Card SwiftUI/ContentView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var points = [CGPoint]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.cyan)
+                .frame(width: 250, height: 250)
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.yellow)
+                .frame(width: 250, height: 250)
+                .overlay {
+                    Image("scratch_image",bundle: nil)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200)
+                }
+                .mask {
+                    Path { path in
+                        path.addLines(points)
+                    }.stroke(style: StrokeStyle(lineWidth: 40, lineCap: .round, lineJoin: .round))
+                }
+                .gesture(
+                    DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                        .onChanged({ value in
+                            points.append(value.location)
+                        })
+                )
         }
         .padding()
     }
